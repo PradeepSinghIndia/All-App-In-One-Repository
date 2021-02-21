8b55715494248ae475f9e63d07102b4ac9fc2455
pradeep <jnu.cdac@gmail.com>
2021-02-22 02:19:53 +0530

    add cart

--- shop/urls.py
+++ shop/urls.py
@@ -4,9 +4,9 @@ from shop import views
 urlpatterns = [
     path(''  ,views.home ,name='home'),
     path('about/' ,views.about ,name='aboutus'),
-    path('contacts/' ,views.contacts ,name='contactus'), 
+    path('contact/' ,views.contact ,name='contactus'), 
     path('search/' ,views.search ,name='search'),
-    path('productView/' ,views.productView ,name='productview'), 
+    path('product/<int:myid>' ,views.productView ,name='productview'), 
     path('tracker/' ,views.tracker ,name='tracking'), 
     path('checkout/' ,views.checkout ,name='checkout'),  
 
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product ,Contact
 from math import ceil
 
 # Create your views here.
@@ -24,13 +24,23 @@ def home(request):
     return render(request,'shop/index.html',params)
 def about(request):
     return render(request,'shop/about.html')
-def contacts(request):
-    return HttpResponse("about contacts") 
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name','')
+        email=request.POST.get('email','')
+        phone=request.POST.get('phone','')
+        desc=request.POST.get('desc','')
+        #print(name,email,phone,desc)
+        contact=Contact(name=name,email=email,phone=phone,desc=desc)
+        contact.save()
+    return render(request,'shop/contact.html')
 def search(request):
-    return HttpResponse("about seraching items")       
-def productView(request):
-    return HttpResponse("about particular products")
+    return render(request,'shop/search.html')      
+def productView(request ,myid):
+    # fetch the product using id
+    product=Product.objects.filter(id=myid)
+    return render(request,'shop/product.html',{'product': product[0]})
 def tracker(request):
-    return HttpResponse("about tracking products")
+    return render(request,'shop/tracker.html')
 def checkout(request):
-    return HttpResponse("about checkouts")        
+    return render(request,'shop/checkout.html')       
